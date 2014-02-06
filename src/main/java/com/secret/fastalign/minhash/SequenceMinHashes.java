9cc3a2cfd662d775d89b992654698797a5413a03
kberlin <kberlin@kbmacbookproretina.home>
2014-02-06 16:24:35 -0500

    improvement for finding order inconsistant matches

--- src/main/java/com/secret/fastalign/minhash/SequenceMinHashes.java
+++ src/main/java/com/secret/fastalign/minhash/SequenceMinHashes.java
@@ -6,23 +6,36 @@ import java.util.Collections;
 import com.secret.fastalign.general.AbstractReducedSequence;
 import com.secret.fastalign.general.Sequence;
 import com.secret.fastalign.utils.Pair;
-import com.secret.fastalign.utils.SortablePair;
 import com.secret.fastalign.utils.Utils;
 
 public final class SequenceMinHashes extends AbstractReducedSequence<MinHash,SequenceMinHashes>
 {
-	private final class SortableIntPair extends SortablePair<Integer, Integer>
+	private final class SortableIntPair implements Comparable<SortableIntPair>
 	{
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = -8427321872814544309L;
-
-		public SortableIntPair(Integer x, Integer y)
+		public final int x;
+		public final int y;
+				
+		public SortableIntPair(int x, int y)
 		{
-			super(x, y);
+			this.x = x;
+			this.y = y;
+		}
+
+
+		@Override
+		public int compareTo(SortableIntPair p)
+		{
+			int result = Integer.compare(this.x, p.x);
+			
+			if (result!=0)
+				return result;
+			
+			return Integer.compare(this.y, p.y);
 		}		
 	}
+	
+	public final static int MAX_SHIFT_ALLOWED = 200;
+	public final static double SHIFT_CONSENSUS_PERCENTAGE = 0.8;
 	
 	private final int completeHash[][];
 	private final int subKmerSize;
@@ -89,16 +102,17 @@ public final class SequenceMinHashes extends AbstractReducedSequence<MinHash,Seq
 		int valid2Lower = 0;
 		int valid2Upper = s.getSequenceLength();
 		int overlapSize = 0;
-		int border = 0;
+		int border = 200;
 		
 		int[][] allKmerHashes = getFullHashes();
 		int[][] sAllKmerHashes = s.getFullHashes();
 		
 		int count = 0;
 		int shift = 0;
+		ArrayList<Integer> posShift = null;
 		for (int repeat=0; repeat<1; repeat++)
 		{
-			ArrayList<Integer> posShift = new ArrayList<Integer>(allKmerHashes.length);
+			posShift = new ArrayList<Integer>(allKmerHashes.length);
 			
 			count = 0;
 			int iter1 = 0;
@@ -139,16 +153,28 @@ public final class SequenceMinHashes extends AbstractReducedSequence<MinHash,Seq
 			valid1Upper = Math.min(getSequenceLength(), s.getSequenceLength()-shift+border);
 			valid2Lower = Math.max(0, shift-border);
 			valid2Upper = Math.min(s.getSequenceLength(), getSequenceLength()+shift+border);
-			overlapSize = valid2Upper-valid2Lower;
+
+			int valid2LowerBorder = Math.max(0, shift);
+			int valid2UpperBorder = Math.min(s.getSequenceLength(), getSequenceLength()+shift);
+			overlapSize = valid2UpperBorder-valid2LowerBorder;
 			
 			//System.out.println("Size1= "+getSequenceLength()+" Lower:"+ valid1Lower+" Upper:"+valid1Upper+" Shift="+shift);
 			//System.out.println("Size2= "+s.getSequenceLength()+" Lower:"+ valid2Lower+" Upper:"+valid2Upper);
 		}
 		
-		double score = 0;
-		if (overlapSize>0)
-			score = (double)count/(double)(overlapSize);
+		//count percent valid shift, there must be a consensus 
+		int percentValid = 0;
+		for (int currShift : posShift)
+		{
+			if (Math.abs(currShift-shift)<=MAX_SHIFT_ALLOWED)
+				percentValid++;
+		}
+		double validShiftPercent = (double)percentValid/(double)posShift.size();
 		
+		double score = 0;
+		if (overlapSize>0 && validShiftPercent>SHIFT_CONSENSUS_PERCENTAGE)
+			score = (double)count/(double)(overlapSize);
+
 		return new Pair<Double, Integer>(score, shift);
 	}
 }
